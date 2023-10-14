3d288dd0755f526c9cce1cf44b3c848d993fd13d
felipegenuino <guntaurus@gmail.com>
2023-10-14 20:03:14 -0300

    update languages select styles

--- src/App.js
+++ src/App.js
@@ -19,12 +19,24 @@ function App() {
 
   }
   return (
-    <div>
-      <button className={currentLang === 'pt' ? 'button-active' : 'button'} onClick={() => changeLang('pt')}>pt</button>
-      <button className={currentLang === 'en' ? 'button-active' : 'button'} onClick={() => changeLang('en')}>en</button>
+    <article>
+      <header>
+        <nav>
+          <button className={currentLang === 'pt' ? 'button-active' : 'button'} onClick={() => changeLang('pt')}>🇧🇷 </button>
+          <button className={currentLang === 'en' ? 'button-active' : 'button'} onClick={() => changeLang('en')}>🇬🇧</button>
+        </nav>
+        <strong>{t('language')}</strong>
+      </header>
 
-      <h1>{t('language')}</h1>
-    </div >
+      <main>
+        <section>
+          <h1>{t('post_title')}</h1>
+          <p>{t('post_description')}</p>
+          <h2>{t('keys_title')}</h2>
+          <p>{t('keys_names')}</p>
+        </section>
+      </main>
+    </article >
   );
 }
 
