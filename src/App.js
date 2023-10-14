1246b5fc9d6c8a8280d13f52bed1ac7fa88f1990
felipegenuino <guntaurus@gmail.com>
2023-10-14 18:28:12 -0300

    update languages button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import './i18n/i18n.jsx'
 import { useTranslation } from 'react-i18next';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   const { t, i18n } = useTranslation()
+  const [currentLang, setCurrentLang] = useState('pt')
+
+  const changeLang = (value) => {
+    i18n
+      .changeLanguage(value)
+      .then(() => {
+        setCurrentLang(value)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+
+  }
   return (
     <div>
+      <button className={currentLang === 'pt' ? 'button-active' : 'button'} onClick={() => changeLang('pt')}>pt</button>
+      <button className={currentLang === 'en' ? 'button-active' : 'button'} onClick={() => changeLang('en')}>en</button>
+
       <h1>{t('language')}</h1>
-    </div>
+    </div >
   );
 }
 
